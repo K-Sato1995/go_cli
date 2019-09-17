f46883614809a02158528287675245e6cf495422
Katsuki <Katsuki_Sato0130@icloud.com>
2019-09-17 20:28:01 +0900

    Add context

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/urfave/cli"
 	"os"
 )
@@ -11,6 +12,22 @@ func main() {
 	app.Name = "sampleApp"
 	app.Usage = "This app echo input arguments"
 	app.Version = "0.0.1"
+
+	app.Action = func(context *cli.Context) error {
+		if context.Bool("cat") {
+			fmt.Println(context.Args().Get(0) + "だにゃん♡")
+		} else {
+			fmt.Println(context.Args().Get(0))
+		}
+		return nil
+	}
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "cat, c",
+			Usage: "Echo with cat",
+		},
+	}
 
 	app.Run(os.Args)
 }
